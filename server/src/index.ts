8293640021968d28f7b8d0198f1171916a71b8f6
Keikomai <keikomai@gmail.com>
2024-01-25 21:15:02 +0100

    feat: added io

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,38 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import helmet from 'helmet'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 dotenv.config()
 const PORT = process.env.PORT
+
+app.use(express.static('public'))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+io.on('connection', (socket) => {
+  console.log('User connected')
+  socket.on('message', (message) => {
+    io.emit('message', message)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+})
+
+function startApp() {
+  try {
+    server.listen(PORT, () => console.log('Server started', PORT, 'port'))
+  } catch (error) {
+    console.log('error')
+  }
+}
+
+startApp()
